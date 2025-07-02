b20ae1aa6b94e91833a6fac9087b37ad218b7195
Sanket Shivaji Yadav <sanketyadav899@gmail.com>
2025-07-02 18:55:50 +0530

    Add the all files to repo

---
+++ script.js
@@ -0,0 +1,116 @@
+/*============= menu icon navbar =====================*/
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+/*============= scroll section action link =============*/
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+
+            let currentLink = document.querySelector('header nav a[href*="' + id + '"]');
+            if (currentLink) currentLink.classList.add('active');
+        }
+    });
+
+    // Sticky navbar
+    let header = document.querySelector('.header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
+
+
+/*============= Dark light mode  =====================*/
+const themeToggle = document.getElementById('theme-toggle');
+
+function toggleTheme() {
+    if (themeToggle.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        document.body.classList.add('dark-mode');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        document.body.classList.remove('dark-mode');
+    }
+}
+
+themeToggle.addEventListener('change', toggleTheme);
+
+
+/*============= Profile slide  =====================*/
+
+const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right');
+const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left');
+let index = 0;
+
+const activePortfolio = () => {
+    const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
+    const portfolioDetails = document.querySelectorAll('.portfolio-detail');
+
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
+    portfolioDetails.forEach(detail => {
+        detail.classList.remove('active');
+    });
+
+    portfolioDetails[index].classList.add('active');
+
+}
+
+arrowRight.addEventListener('click', () => {
+    if (index < 4) {
+        index++;
+        arrowLeft.classList.remove('disabled');
+    }
+    else {
+        index = 5;
+        arrowRight.classList.add('disabled');
+    }
+
+    activePortfolio();
+});
+
+arrowLeft.addEventListener('click', () => {
+    if (index > 1) {
+        index--;
+        arrowRight.classList.remove('disabled');
+    }
+    else {
+        index = 0;
+        arrowLeft.classList.add('disabled');
+    }
+
+    activePortfolio();
+});
+
+
+
+/*============= Scroll reveal  =====================*/
+ScrollReveal({
+    // reset: true,
+    distance: '90px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.heading', { origin: 'top' });
+ScrollReveal().reveal(' .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.about-img img, #codeing,.info', { origin: 'left' });
+ScrollReveal().reveal('.about-content,.img-slide', { origin: 'right' });
